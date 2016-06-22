008a9aa43b88fcd1e4e4b7f9ac50419387b6b763
NorthBei <seekingfor520@gmail.com>
2016-06-22 09:57:06 +0800

    修正排名dialog無法出現的問題
    加回clone functoin
    修正getTime function

--- scripts/index.js
+++ scripts/index.js
@@ -258,7 +258,11 @@ function startCountingDown() {
 
 function getTime()
 {
-	return timer;
+	var time = clone(timer);
+	document.getElementById("timeTemp").textContent = time;
+	
+	var success = document.getElementById("success");
+	success.style.display = "block";
 }
 
 /**********裝潢區**********/
@@ -1222,4 +1226,36 @@ function  initPointerLock(scene,camera) {
 	document.addEventListener("mspointerlockchange", pointerlockchange, false);
 	document.addEventListener("mozpointerlockchange", pointerlockchange, false);
 	document.addEventListener("webkitpointerlockchange", pointerlockchange, false);
+}
+
+function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+	var len
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; ++i) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 }
